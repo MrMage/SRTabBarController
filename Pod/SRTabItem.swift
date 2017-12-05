ea604866e9f52719baf4784505df8c5170b4e670
Daniel Alm <Daniel.Alm@ForumD.net>
2017-12-05 18:01:18 +0100

    Add @objc to another method.

--- Pod/SRTabItem.swift
+++ Pod/SRTabItem.swift
@@ -73,7 +73,7 @@ open class SRTabItem: NSButton {
     
     // MARK: - Actions
     
-    func buttonPressed() {
+    @objc func buttonPressed() {
         delegate?.tabIndexShouldChangeTo(index)
     }
 	
